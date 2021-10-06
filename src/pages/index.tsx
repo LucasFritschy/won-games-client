38e4bb0021b98a7fd58e1fa6aa83d67669a1c877
LucasFritschy <lucasfritschy@gmail.com>
2021-10-06 10:45:31 -0300

    add mappers for homepage data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import highlightMock from 'components/Highlight/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -25,78 +26,22 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 10,
-      banners: banners.map((banner) => ({
-        img: banner.image?.url || null,
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon.text,
-          ribbonColor: banner.ribbon.color,
-          ribbonSize: banner.ribbon.size
-        })
-      })),
+      banners: bannerMapper(banners),
       newGamesTitle: sections?.newGames?.title,
-      newGames: newGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: game.cover?.url || null,
-        price: game.price
-      })),
+      newGames: gamesMapper(newGames),
       mostPopularGamesTitle: sections?.popularGames?.title,
-      mostPopularHighlight: {
-        title: sections?.popularGames!.highlight!.title,
-        subtitle: sections?.popularGames!.highlight!.subtitle,
-        backgroundImage: sections?.popularGames!.highlight!.background?.url,
-        buttonLabel: sections?.popularGames!.highlight!.buttonLabel,
-        buttonLink: sections?.popularGames!.highlight!.buttonLink,
-        floatImage: sections?.popularGames!.highlight!.floatImage?.url,
-        alignment: sections?.popularGames!.highlight!.alignment
-      },
-      mostPopularGames: sections!.popularGames!.games.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: game.cover?.url || null,
-        price: game.price
-      })),
+      mostPopularHighlight: highlightMapper(
+        sections?.popularGames?.highlight || null
+      ),
+      mostPopularGames: gamesMapper(sections!.popularGames!.games),
       upcomingGamesTitle: sections?.upcomingGames?.title,
-      upcomingGames: upcomingGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: game.cover?.url || null,
-        price: game.price
-      })),
-      upcomingHighlight: {
-        title: sections?.upcomingGames!.highlight!.title,
-        subtitle: sections?.upcomingGames!.highlight!.subtitle,
-        backgroundImage: sections?.upcomingGames!.highlight!.background?.url,
-        buttonLabel: sections?.upcomingGames!.highlight!.buttonLabel,
-        buttonLink: sections?.upcomingGames!.highlight!.buttonLink,
-        floatImage: sections?.upcomingGames!.highlight!.floatImage?.url,
-        alignment: sections?.upcomingGames!.highlight!.alignment
-      },
-
+      upcomingGames: gamesMapper(upcomingGames),
+      upcomingHighlight: highlightMapper(
+        sections?.upcomingGames?.highlight || null
+      ),
       freeGamesTitle: sections?.freeGames?.title,
-      freeGames: freeGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: game.cover?.url || null,
-        price: game.price
-      })),
-      freeHighlight: {
-        title: sections?.freeGames!.highlight!.title,
-        subtitle: sections?.freeGames!.highlight!.subtitle,
-        backgroundImage: sections?.freeGames!.highlight!.background?.url,
-        buttonLabel: sections?.freeGames!.highlight!.buttonLabel,
-        buttonLink: sections?.freeGames!.highlight!.buttonLink,
-        floatImage: sections?.freeGames!.highlight!.floatImage?.url,
-        alignment: sections?.freeGames!.highlight!.alignment
-      }
+      freeGames: gamesMapper(freeGames),
+      freeHighlight: highlightMapper(sections?.freeGames?.highlight || null)
     }
   }
 }
