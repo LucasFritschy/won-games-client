257e0442507d230d0dda104e89494b2307901eeb
LucasFritschy <lucasfritschy@gmail.com>
2021-10-05 22:37:33 -0300

    newgames on homepage using dynamic data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,36 +8,35 @@ import { QUERY_HOME } from 'graphql/queries/home'
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
-
-// ATENÇÃO:
-// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
-
-// getStaticProps => gerar estático em build time (gatsby)
-// getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
-// getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  const {
+    data: { banners, newGames }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
 
   return {
     props: {
       revalidate: 10,
-      banners: data.banners.map((banner) => ({
+      banners: banners.map((banner) => ({
         img: banner.image?.url,
         title: banner.title,
         subtitle: banner.subtitle,
-        ...(banner.button && {
-          buttonLabel: banner.button.label,
-          buttonLink: banner.button.link
-        }),
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
         ...(banner.ribbon && {
           ribbon: banner.ribbon.text,
           ribbonColor: banner.ribbon.color,
           ribbonSize: banner.ribbon.size
         })
       })),
-      newGames: gamesMock,
+      newGames: newGames.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: game.cover?.url || null,
+        price: game.price
+      })),
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcomingGames: gamesMock,
