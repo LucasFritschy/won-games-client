dd912f93e549fe12d3169ce6c7c367c7c5e57549
LucasFritschy <lucasfritschy@gmail.com>
2021-10-21 09:35:12 -0300

    add overlay effect on dropdown component

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -30,4 +30,18 @@ describe('<Dropdown />', () => {
     expect(content).toHaveStyle({ opacity: 1 })
     expect(content.getAttribute('aria-hidden')).toBe('false')
   })
+
+  it('should open/close the dropdown when clicking the overlay', () => {
+    const content = screen.getByText(/content/).parentElement!
+    const overlay = content.nextElementSibling
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(overlay).toHaveStyle({ opacity: 1 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('false')
+
+    userEvent.click(overlay!)
+
+    expect(content).toHaveStyle({ opacity: 0 })
+  })
 })
