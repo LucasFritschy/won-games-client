dd8d6ead4218f7ab7134618899e880ab83166f35
LucasFritschy <lucasfritschy@gmail.com>
2021-10-25 10:31:39 -0300

    creating useCart context hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,26 +6,29 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { CartProvider } from 'hooks/use-cart'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApoloState)
 
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="The best Game Stores in the world!"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <CartProvider>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="The best Game Stores in the world!"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CartProvider>
     </ApolloProvider>
   )
 }
