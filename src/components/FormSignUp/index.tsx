046b04c2214e8bbd95e7587afd638cb1aa85a802
LucasFritschy <lucasfritschy@gmail.com>
2021-10-27 09:26:03 -0300

    add createUser muttaion and form in signup

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -4,47 +4,90 @@ import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
 import { FormWrapper, FormLink } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
+import React, { useState } from 'react'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import { useMutation } from '@apollo/client'
+import { MUTATION_REGISTER } from 'graphql/mutations/register'
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="name"
-        placeholder="Name"
-        type="name"
-        icon={<AccountCircle />}
-      />
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <TextField
-        name="confirm-password"
-        placeholder="Confirm password"
-        type="password"
-        icon={<Lock />}
-      />
+const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    username: '',
+    email: '',
+    password: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign up now
-      </Button>
+  const [createUser] = useMutation(MUTATION_REGISTER)
 
-      <FormLink>
-        Already have an account?{' '}
-        <Link href="/sign-in">
-          <a>Sign in</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    createUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  const handleInput = (field: string, value: string) => {
+    setValues({ ...values, [field]: value })
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="username"
+          placeholder="Username"
+          type="text"
+          icon={<AccountCircle />}
+          onInputChange={(v) => {
+            handleInput('username', v)
+          }}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          icon={<Email />}
+          onInputChange={(v) => {
+            handleInput('email', v)
+          }}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          icon={<Lock />}
+          onInputChange={(v) => {
+            handleInput('password', v)
+          }}
+        />
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+          onInputChange={(v) => {
+            handleInput('confirm-password', v)
+          }}
+        />
+
+        <Button type="submit" size="large" fullWidth>
+          Sign up now
+        </Button>
+
+        <FormLink>
+          Already have an account?{' '}
+          <Link href="/sign-in">
+            <a>Sign in</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -1,11 +1,15 @@
 import { screen, render } from 'utils/test-utils'
+import { MockedProvider } from '@apollo/client/testing'
 
 import FormSignUp from '.'
 
 describe('<FormSignUp />', () => {
   it('should render the form', () => {
-    const { container } = render(<FormSignUp />)
-
+    const { container } = render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
     expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
@@ -18,7 +22,11 @@ describe('<FormSignUp />', () => {
   })
 
   it('should render text and link to sign in', () => {
-    render(<FormSignUp />)
+    render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument()
     expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument()
